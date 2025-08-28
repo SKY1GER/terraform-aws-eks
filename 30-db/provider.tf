e32d4f6a7685ea725a866c6dbded84122191beab
SKY1GER <saikumarb9119@gmail.com>
2025-08-28 17:31:09 +0530

    modified source definition in security groups

--- 30-db/provider.tf
+++ 30-db/provider.tf
@@ -2,12 +2,12 @@ terraform {
   required_providers {
     aws = {
       source = "hashicorp/aws"
-      version = "5.48.0"
+      version = "5.92.0"
     }
   }
   backend "s3" {
     bucket = "balasai16"
-    key    = "expense-dev-eks"
+    key    = "expense-dev-db"
     region = "us-east-1"
     dynamodb_table = "daws79s-locking"
   }
