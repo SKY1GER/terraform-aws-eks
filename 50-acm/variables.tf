70afdae6b3e597790cdd470fb67de4398d0c4319
SKY1GER <saikumarb9119@gmail.com>
2025-08-28 15:37:15 +0530

    modified source definition in security groups

--- 50-acm/variables.tf
+++ 50-acm/variables.tf
@@ -16,9 +16,9 @@ variable "common_tags" {
 }
 
 variable "zone_name" {
-  default = "daws78s.online"
+  default = "daws80s.online"
 }
 
 variable "zone_id" {
-  default = "Z08884492QFPW45HM4UQO"
+  default = "Z08266891XBWEUAGKN7EW"
 }
